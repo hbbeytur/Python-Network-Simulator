77f5da96e2c7f58a61b6d077c26b2785013ec42d
hbbeytur <genelP40>
2019-12-31 20:58:07 +0300

    __update__

--- Network.py
+++ Network.py
@@ -1,1 +1,254 @@
-deneme deneme
+import numpy as np
+import math
+import random
+import json
+import analyze
+# import matlab.engine
+
+# eng = matlab.engine.start_matlab()
+
+
+class Network(object):
+    def __init__(self,
+                 num_source = 5,
+               num_server = 1,
+               num_packet = 1000,
+               arrival = ("poisson",[1]*5),
+               service = ("exponential",[0.1]*5),
+               queue = "FCFS",
+               preemption = False,
+               scheduler = "MAF",
+               MTL = False):
+
+
+        self.MTL = MTL
+        self.num_source = num_source
+        self.num_packet = num_packet
+        self.num_server = num_server
+        self.arrivalsetting = arrival
+        self.service = service
+        self.queue = queue
+        self.preemption = preemption
+        self.scheduler = scheduler
+
+        self.packet_generator(num_packet,num_source,arrival)
+        self.Queue = self.Queue(queue,num_source)
+        self.Service = self.Service(num_source,num_packet,num_server,service)
+        self.Scheduler = self.Scheduler(scheduler,preemption,num_source,self)
+
+        self.currenttime = 0
+        self.arrival = []
+        self.story = [[[0,0]] for i in range(num_source)]
+        self.freshstory = [[[0, 0]] for i in range(num_source)]
+        # self.freshness = [True for i in range(num_source)]
+        self.departure = math.inf
+        self.termination = False
+
+    class Queue(object): # Stores arrival time of waiting packets
+        def __init__(self,queue,num_source):
+            self.Qtype = queue
+            self.waiting = [[]]*num_source #TODO story de yaptığımız gibi tanımlamak lazım
+        def add(self,source_id,packet):
+            if self.Qtype == "FCFS":
+                self.waiting[source_id] = [packet] + self.waiting[source_id]
+            elif self.Qtype == "LCFS":
+                self.waiting[source_id] = self.waiting[source_id] + [packet]
+        def delete(self,source_id):
+            if self.waiting[source_id]:
+                return self.waiting[source_id].pop()
+            else: return -1
+        def nextvalue(self,source_id):
+            return self.waiting[source_id][-1]
+        def preempt(self,source_id,packet):
+            self.waiting[source_id] = self.waiting[source_id] + [packet]
+
+
+    def packet_generator(self,num_packet,num_source,arrival,seed = 0):
+        if arrival[0] == "poisson":
+            if self.MTL:
+                arr_matlab = []
+                for i in arrival[1]:
+                    arr_matlab.append(eng.exprnd(float(np.divide(1,i)),1,num_packet))
+                self.arr = np.reshape(arr_matlab,[num_packet,num_source])
+            else:
+                self.arr = np.random.exponential(np.divide(1,arrival[1]),(num_packet,num_source))
+        elif arrival[0] == "deterministic":
+            self.arr = np.ones((num_packet,num_source))*arrival[1]
+
+        self.arr = np.cumsum(self.arr,0)
+        self.generatecontrolinstances(num_source, num_packet,self.arr)
+
+        self.arr = np.insert(self.arr,0,0,axis=0) #TODO: bu gerekli mi
+        self.arr = self.arr.T
+
+    def generatecontrolinstances(self,num_source,num_packet,arr):
+        b = arr.flatten()
+        index = np.mod(b.argsort(),num_source)
+        b.sort()
+        self.controlSteps = np.concatenate([[b],[index]]).T.tolist()
+
+    def store(self,source_id,arrival,departure):
+        self.story[source_id].append([arrival,departure])
+
+    def freshstore(self,source_id,arrival,departure):
+        self.freshstory[source_id].append([arrival,departure])
+
+    class Scheduler(object):
+        def __init__(self,scheduler,preemption,num_source,network):
+            self.scheduler = scheduler
+            self.num_source = num_source
+            self.network = network
+        def nextmove(self,time):
+            if self.scheduler == "MAF":
+                return self.MAF(time)
+            elif self.scheduler == "MAD":
+                return self.MAD(time)
+        def MAF(self,time):
+        # TODO: decide schedule among age-effective and exist packets. Burası daha iyi olabilir
+            inst_age = [time - item[-1][1] for item in self.network.story] # Calculates the inst. age by currentTime - lastDeparture
+            nonempties = list(filter(lambda x: self.network.Queue.waiting[int(x)] != [], range(self.num_source)))
+            cand_ids = []
+            max_age = 0
+            for x in nonempties:
+                if inst_age[x] > max_age:
+                    max_age = inst_age[x]
+                    cand_ids = [x]
+                elif inst_age[x] == max_age:
+                    cand_ids.append(x)
+
+            if len(cand_ids) > 0:
+                return int(random.choice(cand_ids))
+            else:
+                return []
+        def MAD(self,time):
+            pass
+
+    class Service(object):
+        def __init__(self,num_source,num_packet,num_server,service,seed = 15):
+            self.arg = [num_source,num_packet,num_server,service,seed]
+
+            self.id = -1 # Service yapılması muhtemel source ın id'si, -1 ise önceki service tamamlanmış.
+
+            if service[0] == "exponential":
+                self.servicetime = np.random.exponential(np.divide(1,service[1]),(num_packet,num_source))
+            elif service[0] == "determisinistic":
+                self.servicetime = np.ones((num_packet, num_source)) * service[1]
+            self.servicetime = self.servicetime.T.tolist()
+
+        def time(self,source_id):
+            if not self.servicetime[source_id]:
+                self.__init__(*self.arg)
+            return self.servicetime[source_id].pop(0) #TODO: Seed aynı kalıyor sıkıntı olmasın... !!!
+
+    def newService(self,source_id):
+        self.arrival = self.Queue.delete(int(source_id))
+        self.departure = self.currenttime + self.Service.time(source_id)
+        self.Service.id = source_id
+
+    def newServicePreemptive(self,source_id):
+        self.arrival = self.Queue.nextvalue(int(source_id))
+        self.departure = self.currenttime + self.Service.time(source_id)
+        self.Service.id = source_id
+
+    def completeService(self):
+        self.store(int(self.Service.id), self.arrival, self.departure)  # Complete service
+        self.Service.id = -1
+
+    def freshcompleteService(self,source_id):
+        if self.arrival > self.freshstory[int(source_id)][-1][0]:
+            self.freshstore(int(source_id), self.arrival, self.departure)  # Age effective Complete service
+            # self.freshness[int(source_id)] = False
+
+    def controller(self):
+        if not (self.controlSteps or any(self.Queue.waiting)):
+            print("END OF SIMULATION")
+            self.termination = True
+            return 0
+
+        if self.preemption: # LCFS-preemptive için #TODO: subfunctionlar preemptive case için özel olarak düzenlenebilir.
+            source_id = self.Scheduler.nextmove(self.currenttime)
+
+            if source_id == []:
+                self.currenttime, source_id = self.controlSteps.pop(0)
+                self.Queue.add(int(source_id), self.currenttime)
+                # self.freshness[int(source_id)] = True
+            else:
+                arrivaltime = self.Queue.nextvalue(source_id)
+            source_id = int(source_id)
+            self.newServicePreemptive(source_id)
+            if len(self.controlSteps) and self.controlSteps[0][0] < self.departure:
+                (arrivaltime_ib, source_id_ib) = self.controlSteps.pop(0)
+                self.Queue.add(int(source_id_ib), arrivaltime_ib)
+                self.currenttime = arrivaltime_ib
+                new_source_id = self.Scheduler.nextmove(self.currenttime)
+                if not(source_id == source_id_ib or (new_source_id != source_id)):
+                    # NO PREEMPTION
+                    self.currenttime = self.departure
+                    self.Queue.delete(source_id)
+                    self.completeService()
+                    self.freshcompleteService(source_id)
+                # else:
+                #     self.freshness[int(source_id_ib)] = True
+            else:
+                self.currenttime = self.departure
+                self.Queue.delete(source_id)
+                self.completeService()
+                self.freshcompleteService(source_id)
+
+
+        else: # LCFS-nonpreemptive ve FCFS için
+            source_id = self.Scheduler.nextmove(self.currenttime)
+
+            if source_id == []:
+                self.currenttime, source_id = self.controlSteps.pop(0)
+                self.Queue.add(int(source_id), self.currenttime)
+            else:
+                arrivaltime = self.Queue.nextvalue(source_id)
+            source_id = int(source_id)
+            self.newService(source_id)
+            while len(self.controlSteps) and self.controlSteps[0][0] < self.departure:
+                (arrivaltime_ib, source_id_ib) = self.controlSteps.pop(0)
+                self.Queue.add(int(source_id_ib), arrivaltime_ib)
+                # self.idle = False
+            self.currenttime = self.departure  # TODO gerek var mı??
+            self.completeService()
+            self.freshcompleteService(source_id)
+
+
+    def run(self):
+        while not self.termination:
+            self.controller()
+
+if __name__ == "__main__":
+    database = []
+    num_source = 2
+    logging = False
+
+
+    for arrRate in np.linspace(0.01,0.99,num=99):
+        print(arrRate)
+        net = Network(num_source= num_source,queue="FCFS",arrival=("poisson",[arrRate , 0.01]),service=("exponential",[1]*num_source))
+        net.run()
+
+        ## CALCULATE Average Age per user
+        average_age = []
+        for record in net.story:
+            average_age.append(analyze.calculate_average_age2(record))
+
+        print(average_age)
+
+        if logging:
+            database.append({'num_source': net.num_source,
+                             'num_server': net.num_server,
+                             'num_packet': net.num_packet,
+                             'arrival': net.arrivalsetting,
+                             'service': net.service,
+                             'queue': net.queue,
+                             'preemption': net.preemption,
+                             'scheduler': net.scheduler,
+                             'story': net.story})
+    if logging:
+        with open("records/twoUser_FCFS_poisson_exponential.txt","w") as f:
+            json.dump(database,f)
+
+
